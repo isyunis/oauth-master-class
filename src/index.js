67152cb7f98174951e4459072c2bd62f4b27e9fb
isyunis <132367121+isyunis@users.noreply.github.com>
2023-05-03 21:22:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,9 +21,9 @@ window.onload = () => {
       {
         client_id: "179d6d4f23394e908828ef3a56475b1c",
         response_type: "token",
-        redirect_uri: "https://oauth-master-class-9lwk93kyc-isyunis.vercel.app/token.html",
+        redirect_uri: "https://oauth-master-class-omega.vercel.app/token.html",
       },
-      "https://oauth-master-class-9lwk93kyc-isyunis.vercel.app"
+      "https://oauth-master-class-omega.vercel.app"
     )
       .then(({ handler }) => handler())
       .then(async (data) => {
@@ -40,9 +40,9 @@ window.onload = () => {
       {
         client_id: "179d6d4f23394e908828ef3a56475b1c",
         response_type: "token",
-        redirect_uri: "https://oauth-master-class-9lwk93kyc-isyunis.vercel.app/token.html",
+        redirect_uri: "https://oauth-master-class-omega.vercel.app/token.html",
       },
-      "https://oauth-master-class-9lwk93kyc-isyunis.vercel.app",
+      "https://oauth-master-class-omega.vercel.app",
       {
         parentId: "buttonContainer",
         view: "button",
